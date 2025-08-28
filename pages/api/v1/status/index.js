a9935259f53caf05bd6fd6c68ecfc4afd823578b
Filetzin <bruno.fiore.99@gmail.com>
2025-08-28 20:06:51 +0000

    add services script

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "jsconfig.json/infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
