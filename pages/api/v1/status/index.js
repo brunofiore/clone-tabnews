aee154cd36ff37da417baa9e8fcdb31463d29b38
Filetzin <bruno.fiore.99@gmail.com>
2025-09-03 19:08:32 +0000

    Add versão do banco, maximo de conexões e conexçoes usadas ao endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
+import { TRACE_OUTPUT_VERSION } from "next/dist/shared/lib/constants";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status("200").json({ chave: "ok!" });
+  const updatedAt = new Date().toISOString();
+  const dbName = process.env.POSTGRES_DB;
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbMaxConnection = await database.query("SHOW max_connections;");
+  const dbUsedConnection = await database.query({
+    text: "SELECT COUNT(*)::int AS total_connections FROM pg_stat_activity WHERE state = 'active' AND datname = $1;",
+    values: [dbName],
+  });
+  response.status("200").json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        db_version: dbVersion.rows[0].server_version,
+        db_max: parseInt(dbMaxConnection.rows[0].max_connections),
+        db_used: dbUsedConnection.rows[0].total_connections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,33 @@
+const { resolve } = require("styled-jsx/css");
+
 test("GET no /api/v1/status deve retornar 200", async () => {
+  //Valida resposta 200 do http
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  //Valida recebimento do updatedAt da API
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  //Valida a versão do banco de dados retornada
+  console.log(responseBody);
+  expect(responseBody.dependencies.database.db_version).toEqual(
+    expect.any(String),
+  );
+  expect(responseBody.dependencies.database.db_version.length).toBeGreaterThan(
+    0,
+  );
+
+  //valida a quantidade maxima de conexões ao banco
+
+  expect(responseBody.dependencies.database.db_max).toEqual(expect.any(Number));
+  expect(responseBody.dependencies.database.db_max).toBeGreaterThan(0);
+
+  //valida númerom de conexoes ativas do banco
+
+  expect(responseBody.dependencies.database.db_used).toEqual(
+    expect.any(Number),
+  );
+  expect(responseBody.dependencies.database.db_used).toBeGreaterThan(0);
 });
