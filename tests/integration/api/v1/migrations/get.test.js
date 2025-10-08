b05a67a9d4f23fce5077867dda8b39a9fa065036
Filetzin <bruno.fiore.99@gmail.com>
2025-10-08 14:34:36 +0000

    add texto erro get migrations

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -10,6 +10,7 @@ beforeAll(async () => {
 test("GET no /api/v1/migrations deve retornar 200", async () => {
   //Valida resposta 200 do http
   const response = await fetch("http://localhost:3000/api/v1/migrations");
+
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
