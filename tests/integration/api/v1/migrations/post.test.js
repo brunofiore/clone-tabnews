fdbcc2b70dd686daba5f5adb88d59a0e273aa20a
Filetzin <bruno.fiore.99@gmail.com>
2025-10-28 18:57:09 +0000

    test: rename all discriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,27 +6,39 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST no /api/v1/migrations deve retornar 200", async () => {
-  //Valida resposta 200 do http
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Usuário anonimo", () => {
+    describe("Rodar migrações pendentes", () => {
+      test("pela primeira vez", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1.status).toBe(201);
+
+        const response1Body = await response1.json();
+
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBeGreaterThan(0);
+        //console.log(response1Body);
+      });
+      test("pela segunda vez", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        const response2Body = await response2.json();
+
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+        //console.log(response2Body);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const response1Body = await response1.json();
-
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
-  //console.log(response1Body);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
-  //console.log(response2Body);
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,34 +4,40 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET no /api/v1/status deve retornar 200", async () => {
-  //Valida resposta 200 do http
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-  //Valida recebimento do updatedAt da API
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+describe("GET /api/v1/status", () => {
+  describe("Usuário anonimo", () => {
+    test("Obtendo status atual do sistema", async () => {
+      //Valida resposta 200 do http
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
+      //Valida recebimento do updatedAt da API
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  //Valida a versão do banco de dados retornada
+      //Valida a versão do banco de dados retornada
 
-  expect(responseBody.dependencies.database.db_version).toEqual(
-    expect.any(String),
-  );
-  expect(responseBody.dependencies.database.db_version.length).toBeGreaterThan(
-    0,
-  );
+      expect(responseBody.dependencies.database.db_version).toEqual(
+        expect.any(String),
+      );
+      expect(
+        responseBody.dependencies.database.db_version.length,
+      ).toBeGreaterThan(0);
 
-  //valida a quantidade maxima de conexões ao banco
+      //valida a quantidade maxima de conexões ao banco
 
-  expect(responseBody.dependencies.database.db_max).toEqual(expect.any(Number));
-  expect(responseBody.dependencies.database.db_max).toBeGreaterThan(0);
+      expect(responseBody.dependencies.database.db_max).toEqual(
+        expect.any(Number),
+      );
+      expect(responseBody.dependencies.database.db_max).toBeGreaterThan(0);
 
-  //valida númerom de conexoes ativas do banco
+      //valida númerom de conexoes ativas do banco
 
-  expect(responseBody.dependencies.database.db_used).toEqual(
-    expect.any(Number),
-  );
-  expect(responseBody.dependencies.database.db_used).toBeGreaterThan(0);
+      expect(responseBody.dependencies.database.db_used).toEqual(
+        expect.any(Number),
+      );
+      expect(responseBody.dependencies.database.db_used).toBeGreaterThan(0);
+    });
+  });
 });
