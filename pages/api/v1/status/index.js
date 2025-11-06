ce5cb6810b0b2a16e17cf74bb301667a222e46cb
Filetzin <bruno.fiore.99@gmail.com>
2025-11-06 14:22:17 +0000

    feat: add internal server error to custom errors

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,25 +1,35 @@
 import database from "infra/database.js";
-
+import { InternalServerError } from "infra/errors";
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
-  const dbName = process.env.POSTGRES_DB;
-  const dbVersion = await database.query("SHOW server_version;");
-  const dbMaxConnection = await database.query("SHOW max_connections;");
-  const dbUsedConnection = await database.query({
-    text: "SELECT COUNT(*)::int AS total_connections FROM pg_stat_activity WHERE state = 'active' AND datname = $1;",
-    values: [dbName],
-  });
-  response.status("200").json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        db_version: dbVersion.rows[0].server_version,
-        db_max: parseInt(dbMaxConnection.rows[0].max_connections),
-        db_used: dbUsedConnection.rows[0].total_connections,
-        db_name: dbName,
+  try {
+    const updatedAt = new Date().toISOString();
+    const dbName = process.env.POSTGRES_DB;
+    const dbVersion = await database.query("SHOW server_version;");
+    const dbMaxConnection = await database.query("SHOW max_connections;");
+    const dbUsedConnection = await database.query({
+      text: "SELECT COUNT(*)::int AS total_connections FROM pg_stat_activity WHERE state = 'active' AND datname = $1;",
+      values: [dbName],
+    });
+    response.status("200").json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          db_version: dbVersion.rows[0].server_version,
+          db_max: parseInt(dbMaxConnection.rows[0].max_connections),
+          db_used: dbUsedConnection.rows[0].total_connections,
+          db_name: dbName,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\n Erro dentro do catch do controller:");
+    console.log(publicErrorObject);
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
