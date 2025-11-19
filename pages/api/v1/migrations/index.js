adf47aba91a8a7d3c72a8986475112540809129f
Filetzin <bruno.fiore.99@gmail.com>
2025-11-19 14:16:49 +0000

    refactor: make api/v1/migrations use migrator model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,6 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
-import { InternalServerError, MethodNotAlowwedError } from "infra/errors";
+import controller from "infra/controller";
+import migrator from "models/migrator";
 
 const router = createRouter();
 
@@ -10,63 +8,20 @@ router.get(getHandler);
 router.post(postHandler);
 
 export default router.handler({
-  onNoMatch: onNoMatchHandler,
-  onError: onErrorHandler,
+  onNoMatch: controller.onNoMatchHandler,
+  onError: controller.onErrorHandler,
 });
 
-function onNoMatchHandler(request, response) {
-  const publicErrorObject = new MethodNotAlowwedError();
-  response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
-function onErrorHandler(error, request, response) {
-  const publicErrorObject = new InternalServerError({
-    cause: error,
-  });
-
-  console.log("\n Erro dentro do catch do next-connect:");
-  console.log(publicErrorObject);
-  response.status(500).json(publicErrorObject);
-}
-
-const defaultMigrationsOptions = {
-  dryRun: true,
-  dir: resolve("infra", "migrations"),
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
-
 async function getHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dbClient,
-    });
-
-    return response.status("200").json(pendingMigrations);
-  } finally {
-    await dbClient.end();
-  }
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return response.status(200).json(pendingMigrations);
 }
+
 async function postHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
+  const migratedMigrations = await migrator.runPendigMigrations();
 
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-      dbClient,
-    });
-
-    if (migratedMigrations.length > 0) {
-      return response.status("201").json(migratedMigrations);
-    }
-    return response.status("200").json(migratedMigrations);
-  } finally {
-    await dbClient.end();
+  if (migratedMigrations.length > 0) {
+    return response.status("201").json(migratedMigrations);
   }
+  return response.status("200").json(migratedMigrations);
 }
